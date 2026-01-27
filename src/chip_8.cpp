00937f9f6e985aa0d8aab17a2dbf89087a6c3a22
Areeq <imranareeq@gmail.com>
2026-01-27 11:59:51 +1100

    feat: implemented up to op_8XY4

--- src/chip_8.cpp
+++ src/chip_8.cpp
@@ -1,27 +1,131 @@
 #include "chip_8.h"
+
 #include <fstream>
 #include <ranges>
 
-namespace c8
-{
-  auto chip_8::load_rom(const std::string file_path) -> void
-  {
-    std::ifstream file(file_path, std::ios::binary | std::ios::ate);
+namespace c8 {
+auto chip_8::load_rom(const std::string file_path) -> void {
+  std::ifstream file(file_path, std::ios::binary | std::ios::ate);
 
-    if (file.is_open())
-    {
-      auto size = file.tellg();
-      auto buffer = std::string{};
-      buffer.reserve(size);
-      file.seekg(0);
+  if (file.is_open()) {
+    auto size = file.tellg();
+    auto buffer = std::string{};
+    buffer.reserve(size);
+    file.seekg(0);
 
-      file.read(buffer.data(), size);
-      file.close();
+    file.read(buffer.data(), size);
+    file.close();
 
-      for (auto i : std::views::iota(0, size))
-      {
-        this->memory[ROM_START_ADDRESS + i] = buffer[i];
-      }
+    for (auto i : std::views::iota(0, size)) {
+      memory[ROM_START_ADDRESS + i] = buffer[i];
     }
+  } else {
+    // TODO: maybe enumerate the opcodes or something.
+    memory[ROM_START_ADDRESS] = 0x00;
+    memory[ROM_START_ADDRESS + 1] = 0xEE;
   }
 }
+
+/* ========== OP CODES ========== */
+auto chip_8::op_001N() -> void {
+  // TODO: move to the end??
+  return;
+}
+
+auto chip_8::op_00E0() -> void {
+  display_memory.fill(0);
+}
+
+auto chip_8::op_00EE() -> void {
+  program_counter = stack[--pointer];
+}
+
+auto chip_8::op_1NNN() -> void {
+  program_counter = opcode & NNN_MASK;
+}
+
+auto chip_8::op_2NNN() -> void {
+  stack[pointer++] = program_counter;
+  program_counter = opcode && NNN_MASK;
+}
+
+auto chip_8::op_3XNN() -> void {
+  const uint8_t VX = (opcode & VX_MASK) >> VX_SHIFT;
+  const uint8_t NN = opcode & NN_MASK;
+
+  if (registers[VX] == NN) {
+    program_counter += 2;
+  }
+}
+
+auto chip_8::op_4XNN() -> void {
+  const uint8_t VX = (opcode & VX_MASK) >> VX_SHIFT;
+  const uint8_t NN = opcode & NN_MASK;
+
+  if (registers[VX] != NN) {
+    program_counter += 2;
+  }
+}
+
+auto chip_8::op_5XY0() -> void {
+  const uint8_t VX = (opcode & VX_MASK) >> VX_SHIFT;
+  const uint8_t VY = (opcode & VY_MASK) >> VY_SHIFT;
+
+  if (registers[VX] = registers[VY]) {
+    program_counter += 2;
+  }
+}
+
+auto chip_8::op_6XNN() -> void {
+  const uint8_t VX = (opcode & VX_MASK) >> VX_SHIFT;
+  const uint8_t NN = opcode & NN_MASK;
+
+  registers[VX] = NN;
+}
+
+auto chip_8::op_7XNN() -> void {
+  const uint8_t VX = (opcode & VX_MASK) >> VX_SHIFT;
+  const uint8_t NN = opcode & NN_MASK;
+
+  registers[VX] += NN;
+}
+
+auto chip_8::op_8XY0() -> void {
+  const uint8_t VX = (opcode & VX_MASK) >> VX_SHIFT;
+  const uint8_t VY = (opcode & VY_MASK) >> VY_SHIFT;
+
+  registers[VX] = registers[VY];
+}
+
+auto chip_8::op_8XY1() -> void {
+  const uint8_t VX = (opcode & VX_MASK) >> VX_SHIFT;
+  const uint8_t VY = (opcode & VY_MASK) >> VY_SHIFT;
+
+  registers[VX] |= registers[VY];
+}
+
+auto chip_8::op_8XY2() -> void {
+  const uint8_t VX = (opcode & VX_MASK) >> VX_SHIFT;
+  const uint8_t VY = (opcode & VY_MASK) >> VY_SHIFT;
+
+  registers[VX] &= registers[VY];
+}
+
+auto chip_8::op_8XY3() -> void {
+  const uint8_t VX = (opcode & VX_MASK) >> VX_SHIFT;
+  const uint8_t VY = (opcode & VY_MASK) >> VY_SHIFT;
+
+  registers[VX] ^= registers[VY];
+}
+
+auto chip_8::op_8XY4() -> void {
+  // TODO: add overflow
+  const uint8_t VX = (opcode & VX_MASK) >> VX_SHIFT;
+  const uint8_t VY = (opcode & VY_MASK) >> VY_SHIFT;
+
+  const uint16_t sum = registers[VX] + registers[VY];
+
+  registers[VF_ADDRESS] = sum > REGISTER_MAX_VALUE ? 1 : 0;
+}
+
+}  // namespace c8
--- src/chip_8.h
+++ src/chip_8.h
@@ -1,81 +1,87 @@
-#include <cstdint>
 #include <array>
+#include <cstdint>
 #include <string>
 #include <vector>
 
-namespace c8
-{
-  class chip_8
-  {
-  private:
-    static constexpr uint8_t NUM_REGISTERS = 16;
-    static constexpr uint8_t STACK_SIZE = 16;
-    static constexpr uint8_t NUM_INPUT_KEYS = 16;
-    static constexpr uint8_t DISPLAY_HEIGHT = 32;
-    static constexpr uint8_t DISPLAY_WIDTH = 64;
-    static constexpr uint8_t FONTSET_SIZE = 80;
-    static constexpr uint16_t ROM_START_ADDRESS = 0x200;
-    static constexpr uint16_t MEMORY_BYTES = 4096;
+namespace c8 {
+class chip_8 {
+ private:
+  static constexpr uint8_t VF_ADDRESS = 0xF;
+  static constexpr uint8_t NUM_REGISTERS = 16;
+  static constexpr uint8_t STACK_SIZE = 16;
+  static constexpr uint8_t NUM_INPUT_KEYS = 16;
+  static constexpr uint8_t DISPLAY_HEIGHT = 32;
+  static constexpr uint8_t DISPLAY_WIDTH = 64;
+  static constexpr uint8_t FONTSET_SIZE = 80;
+  static constexpr uint8_t REGISTER_MAX_VALUE = 255;
+  static constexpr uint16_t ROM_START_ADDRESS = 0x200;
+  static constexpr uint16_t MEMORY_BYTES = 4096;
 
-    std::array<uint8_t, NUM_REGISTERS> registers{};
-    std::array<uint8_t, MEMORY_BYTES> memory{};
-    std::array<uint8_t, NUM_INPUT_KEYS> input_keys{};
-    std::array<uint16_t, STACK_SIZE> stack{};
-    std::array<uint32_t, DISPLAY_WIDTH * DISPLAY_HEIGHT> display_memory{};
+  static constexpr uint8_t VY_SHIFT = 4;
+  static constexpr uint8_t VX_SHIFT = 8;
+  static constexpr uint16_t NNN_MASK = 0x0FFF;
+  static constexpr uint16_t VX_MASK = 0x0F00;
+  static constexpr uint16_t VY_MASK = 0x00F0;
+  static constexpr uint16_t NN_MASK = 0x00FF;
 
-    uint8_t pointer{};
-    uint8_t delay_timer{};
-    uint8_t sound_timer{};
-    uint16_t index_register{};
-    uint16_t program_counter{};
+  std::array<uint8_t, NUM_REGISTERS> registers{};
+  std::array<uint8_t, MEMORY_BYTES> memory{};
+  std::array<uint8_t, NUM_INPUT_KEYS> input_keys{};
+  std::array<uint16_t, STACK_SIZE> stack{};
+  std::array<uint32_t, DISPLAY_WIDTH * DISPLAY_HEIGHT> display_memory{};
 
-  public:
-    auto load_rom(const std::string file_path) -> void;
+  uint8_t pointer{};
+  uint8_t delay_timer{};
+  uint8_t sound_timer{};
+  uint16_t index_register{};
+  uint16_t program_counter{};
+  uint16_t opcode{};
 
-    /*
-      op codes. docs at:
-      https://github.com/trapexit/chip-8_documentation
-    */
+ public:
+  auto load_rom(const std::string file_path) -> void;
 
-    auto op_001N() -> void; // EXIT N
-    auto op_00E0() -> void; // CLS
-    auto op_00EE() -> void; // RET
-    auto op_00FA() -> void; // COMPAT
-    auto op_0NNN() -> void; // CALL NNN
-    auto op_1NNN() -> void; // JMP NNN
-    auto op_2NNN() -> void; // CALL NNN
-    auto op_3XNN() -> void; // SE VX, NN
-    auto op_4xNN() -> void; // SNE VX, NN
-    auto op_5XY0() -> void; // SE VX, VY
-    auto op_6XNN() -> void; // LD VX, NN
-    auto op_7XNN() -> void; // ADD VX, NN
-    auto op_8XY0() -> void; // LD VX, VY
-    auto op_8XY1() -> void; // OR VX, VY
-    auto op_8XY2() -> void; // AND VX, VY
-    auto op_8XY3() -> void; // XOR VX, VY
-    auto op_8XY4() -> void; // ADD VX, VY
-    auto op_8XY5() -> void; // SUB VX, VY
-    auto op_8XY6() -> void; // SHR VX, VY
-    auto op_8XY7() -> void; // SUBN VX, VY
-    auto op_8XYE() -> void; // SHL VX, VY
-    auto op_9XY0() -> void; // SNE VX, VY
-    auto op_ANNN() -> void; // LD I, NNN
-    auto op_BNNN() -> void; // JMP V0, NNN
-    auto op_CXNN() -> void; // RND VX, NN
-    auto op_DXYN() -> void; // DRW VX, VY, N
-    auto op_EX9E() -> void; // SKP VX
-    auto op_EXA1() -> void; // SKNP VX
-    auto op_FX07() -> void; // LD VX, DT
-    auto op_FX0A() -> void; // LD VX, KEY
-    auto op_FX15() -> void; // LD DT, VX
-    auto op_FX18() -> void; // LD ST, VX
-    auto op_FX1E() -> void; // ADD I, VX
-    auto op_FX29() -> void; // LD I, FONT(VX)
-    auto op_FX33() -> void; // BCD VX
-    auto op_FX55() -> void; // LD [I], VX
-    auto op_FX65() -> void; // LD VX, [I]
+  /*
+    op codes docs at:
+    https://github.com/trapexit/chip-8_documentation
+  */
 
-  };
+  auto op_001N() -> void;  // EXIT N
+  auto op_00E0() -> void;  // CLS
+  auto op_00EE() -> void;  // RET
+  auto op_0NNN() -> void;  // CALL NNN
+  auto op_1NNN() -> void;  // JMP NNN
+  auto op_2NNN() -> void;  // CALL NNN
+  auto op_3XNN() -> void;  // SE VX, NN
+  auto op_4XNN() -> void;  // SNE VX, NN
+  auto op_5XY0() -> void;  // SE VX, VY
+  auto op_6XNN() -> void;  // LD VX, NN
+  auto op_7XNN() -> void;  // ADD VX, NN
+  auto op_8XY0() -> void;  // LD VX, VY
+  auto op_8XY1() -> void;  // OR VX, VY
+  auto op_8XY2() -> void;  // AND VX, VY
+  auto op_8XY3() -> void;  // XOR VX, VY
+  auto op_8XY4() -> void;  // ADD VX, VY
+  auto op_8XY5() -> void;  // SUB VX, VY
+  auto op_8XY6() -> void;  // SHR VX, VY
+  auto op_8XY7() -> void;  // SUBN VX, VY
+  auto op_8XYE() -> void;  // SHL VX, VY
+  auto op_9XY0() -> void;  // SNE VX, VY
+  auto op_ANNN() -> void;  // LD I, NNN
+  auto op_BNNN() -> void;  // JMP V0, NNN
+  auto op_CXNN() -> void;  // RND VX, NN
+  auto op_DXYN() -> void;  // DRW VX, VY, N
+  auto op_EX9E() -> void;  // SKP VX
+  auto op_EXA1() -> void;  // SKNP VX
+  auto op_FX07() -> void;  // LD VX, DT
+  auto op_FX0A() -> void;  // LD VX, KEY
+  auto op_FX15() -> void;  // LD DT, VX
+  auto op_FX18() -> void;  // LD ST, VX
+  auto op_FX1E() -> void;  // ADD I, VX
+  auto op_FX29() -> void;  // LD I, FONT(VX)
+  auto op_FX33() -> void;  // BCD VX
+  auto op_FX55() -> void;  // LD [I], VX
+  auto op_FX65() -> void;  // LD VX, [I]
+};
 
-  // non member funcs
-}
+// non member funcs
+}  // namespace c8
